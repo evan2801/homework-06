b5945f685414f09c25908614577da74676f0ff32
evan stein <evanstein28@gmail.com>
2020-01-14 21:08:24 -0500

    pushing work

---
+++ script.js
@@ -0,0 +1,126 @@
+var appid = "809ca8cb6186d89628eb22d129fb1ffa";
+var intervalId;
+
+// set up interval to update time every second
+intervalId = setInterval(function() {
+    var currentTime = moment().format('dddd, MMMM YYYY [at] hh:mm:ss a');
+    $("#current-time").text(currentTime);
+}, 1000);
+
+function getWeatherInfo(event) {
+    event.preventDefault()
+    var placeToSearch = $('#weather-input').val()
+
+    console.log(placeToSearch)
+    getForecastInfo(placeToSearch)
+
+   var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${placeToSearch}&APPID=${appid}`
+
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response)
+        var tempInKelvin = response.main.temp
+        var tempInFahrenheit = convertKToF(tempInKelvin)
+        console.log(tempInFahrenheit)
+        var weather = response.weather[0].description
+        console.log(weather)
+        var speed = response.wind.speed
+        console.log(speed)
+        var humidity = response.main.humidity
+        console.log(humidity)
+
+        var weatherDiv = $("<div class='weather'>");
+
+        
+
+        var pOne = $("<p>").text("temp: " + tempInFahrenheit);
+
+
+        weatherDiv.append(pOne);
+
+        var pTwo = $("<p>").text("description: " + weather);
+
+        weatherDiv.append(pTwo)
+
+        var pThree = $("<p>").text("wind: " + speed);
+
+        weatherDiv.append(pThree)  
+        
+        var pFour = $("<p>").text("humidity: " + humidity);
+
+        weatherDiv.append(pFour)  
+
+
+        $("#weather-view").prepend(weatherDiv)
+
+
+    })
+}
+
+function getForecastInfo(city) {
+    
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${appid}`
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+        //weather for 5 days
+        var tempList = response.list
+
+        for (var i = 0; i < tempList.length; i++) {
+            var tempMin = response.list[i].main.temp_min
+            tempMin = convertKToF(tempMin)
+            console.log(tempMin)
+            var tempMax = response.list[i].main.temp_max
+            tempMax = convertKToF(tempMax)
+            var dateTime = response.list[i].dt_txt
+            console.log(dateTime)
+
+            var forecastDiv = $("<div class='forecast'>");
+
+
+            var pOne = $("<p>").text("low of: " + tempMin);
+
+            forecastDiv.append(pOne)
+
+            var pTwo = $("<p>").text("high of: " + tempMax);
+
+            forecastDiv.append(pTwo)
+
+            var pThree = $("<p>").text("date: " + dateTime);
+
+            forecastDiv.append(pThree)
+
+
+
+            $("#forecast-view").prepend(forecastDiv)
+
+        }
+        // first temp high
+  
+    
+        // second  temp low
+
+
+        //date of temp
+    })
+
+
+}
+
+function convertKToF(temp) {
+    return temp * 9 / 5 - 459.67
+}
+
+
+
+$("#weather-form").on('submit', getWeatherInfo)
